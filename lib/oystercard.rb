b22591206b40faf4cc2a2363894627b48e2bf3e3
Gadiza Zerari <gadizaz@hotmail.com>
2018-03-27 12:00:29 +0100

    Challenge 6: including maximum_balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,7 @@
 class Oystercard
+
+  MAXIMUM_BALANCE = 90
+
   attr_reader :balance
 
   def initialize
@@ -6,6 +9,7 @@ class Oystercard
   end
 
   def top_up(value)
+    fail 'Maximum balance of #{maximum_balance} exceeded' if balance + value > MAXIMUM_BALANCE
     @balance = @balance + value
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,8 +7,14 @@ describe Oystercard do
   describe '#top-up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
     it 'can top up balance' do
-      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5 
+      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
     end
+    it 'raises an error if maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+    end
+
   end
 
 end
